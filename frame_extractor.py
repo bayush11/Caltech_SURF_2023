6f14802de6bff6d5cec0f222e64f51871171ad8b
bayush11 <70395352+bayush11@users.noreply.github.com>
2023-08-22 09:12:28 -0700

    Update frame_extractor.py

--- frame_extractor.py
+++ frame_extractor.py
@@ -1,6 +1,10 @@
+#This code is used to extract frames from a given video. Down below you can see the 
+#video being accessed as well as the nth frame of all of the frames being extracted. 
+#You have the ability to change this value to whatever you want.
+
 import cv2
 
-capture = cv2.VideoCapture(r"C:\Users\ayush\Downloads\beaker_good_fast_sands_trimmed.mp4")
+capture = cv2.VideoCapture(r"C:\Users\ayush\Downloads\beaker_good_fast_sands_trimmed.mp4") #reads the video file
 
 currentframe = 0
 counter = 10 #captures every nth image
@@ -11,13 +15,13 @@ while (True):
     ret, frame = capture.read()
 
     if ret and counter == 0:
-        name = r"C:\Users\ayush\Documents\IMG_FROM_VIDEO\frame" + str(currentframe) + '.jpg'
+        name = r"C:\Users\ayush\Documents\IMG_FROM_VIDEO\frame" + str(currentframe) + '.jpg' #this is the name of the frame
         print('Creating...' + name)
 
         cv2.imwrite(name, frame)
 
         currentframe += 1
-        counter = 10
+        counter = 10 #nth fame is taken and this number can be changed to any.
     else:
         break
 
